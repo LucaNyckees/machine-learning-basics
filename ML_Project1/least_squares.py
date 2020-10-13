da2f4f90e93a19ac73d46f2cd815237d890fe0dd
LucaNyckees <55453275+LucaNyckees@users.noreply.github.com>
2020-10-13 19:32:46 +0200

    Update least_squares.py

--- ML_Project1/least_squares.py
+++ ML_Project1/least_squares.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Oct  3 13:41:31 2020
-
-@author: nyckeesluca
-"""
-
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -13,11 +5,10 @@ import matplotlib.pyplot as plt
 
 def least_squares(y, tx):
     N=y.shape[0]
-    X=tx.T
+    X=tx
     A = np.linalg.inv(X.T.dot(X))
-    w = (A.dot(X)).dot(y)
-    #errors = (y-X.cdot(w))**2
-    #MSE = np.sum(errors)/N
-    #return w, MSE
-    return w,y-X.cdot(w)
+    w = (A.dot(X.T)).dot(y)
+    errors = (y-X.dot(w))**2
+    MSE = np.sum(errors)/N
+    return w, MSE
     
