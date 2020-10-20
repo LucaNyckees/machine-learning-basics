4a4c0770512b75d07b219b2f55f39cbca9446513
LucaNyckees <55453275+LucaNyckees@users.noreply.github.com>
2020-10-20 14:28:50 +0200

    Update logistic_regression.py

--- ML_Project1/logistic_regression.py
+++ ML_Project1/logistic_regression.py
@@ -10,7 +10,9 @@ def calculate_loss(y, tx, w):
     L=0
     X=tx
     for i in range(y.shape[0]):
-        L=L-y[i]*X[i,:].dot(w)+np.log(1+np.exp(X[i,:].dot(w)))
+        xi=X[i,:]
+        L=L-y[i]*xi.T.dot(w)
+        L=L+np.log(1+np.exp(xi.T.dot(w)))
     return -L
 
 
@@ -18,14 +20,17 @@ def calculate_gradient(y, tx, w):
     g=0
     X=tx
     for i in range(y.shape[0]):
-        g=g-y[i]*X[:,i]+X[:,i].dot(sigmoid(X[i,:].dot(w)))
+        xi=X[i,:]
+        g=g-y[i]*xi
+        product=sigmoid(xi.T.dot(w))
+        g=g+xi.dot(product)
     return g
 
 
 def learning_by_gradient_descent(y, tx, w, gamma):
     w_new=w-gamma*calculate_gradient(y, tx, w)
     loss=compute_loss(y,tx,w_new)
-    return w,loss
+    return w_new,loss
 
 
 def de_standardize(x, mean_x, std_x):
