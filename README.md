06e9aa68013a4f6dda1be256030d9d755c1acb4e
Luca Nyckees <luca.nyckees@epfl.ch>
2023-03-30 16:10:32 +0200

    Added: structure of repo re-viewed

--- README.md
+++ README.md
@@ -1,29 +1,71 @@
 # Machine Learning (CS-433)
+
 ## The Higgs Boson Project
 
-### Team members
-Raphaël Mirallié\
-Luca Nyckees\
-Clarisse Schumer
+### Team
 
-### Requirements
-* `Numpy`
-* `Matplotlib`
+Raphaël Mirallié, Luca Nyckees, Clarisse Schumer
 
-### Files
-`ML_Project1/Projet/scripts`contains some `.py` and `.ipynb` files, including the following :
-  - `helper_implementations.py`, defining the basic tools used in `implementations.py`,
-  - `implementations.py`, where the main machine learning algorithms are implemented,
-  - `cleaning_data.py`, where do a bit of initial data analysis (splitting methods, standardization, treating outliers),
-  - `cross_validation.py`, where we set some important cross-validation methods to optimize the choice of hyperparameters.
-  - Some notebooks found at the extension `ML_Project1/Projet/scripts/.ipynb_checkpoints`, where all implemented methods are put to use, the training of tha data and the producing of the prediction files take place.
-  
-### Submissions
-`ML_Project1/Projet/submission` contains a list of `.csv`submission files, consisting of predictions on the given dataset.
-  
-  
-### Contact us
-raphael.mirallie@epfl.ch\
-luca.nyckees@epfl.ch\
-clarisse.schumer@epfl.ch
+---
 
+## Virtual environment
+
+---
+
+Use the following command lines to create and use venv python package:
+
+```
+python3.10 -m venv venv
+```
+
+Then use the following to activate the environment:
+
+```
+source venv/bin/activate
+```
+
+You can now use pip to install any packages you need for the project and run python scripts, usually through a `requirements.txt`:
+
+```
+python -m pip install -r requirements.txt
+```
+
+When you are finished, you can stop the environment by running:
+
+```
+deactivate
+```
+
+---
+
+## Project Organization
+
+---
+
+    ├── README.md          -- Top-level README.
+    │
+    ├── notebooks          -- Jupyter notebooks.
+    │
+    ├── files              -- Notes and report (Latex, pdf).
+    │
+    ├── data               -- data sets.
+    │
+    ├── requirements.txt   -- Requirements file for reproducibility.
+    │
+    └── src                -- Source code for use in this project.
+        │
+        ├── ...
+
+### `src`
+
+`src` contains `.py` files, including the following.
+
+- `helper_implementations.py`, defining the basic tools used in `implementations.py`,
+- `implementations.py`, where the main machine learning algorithms are implemented,
+- `cleaning_data.py`, where do a bit of initial data analysis (splitting methods, standardization, treating outliers),
+- `cross_validation.py`, where we set some important cross-validation methods to optimize the choice of hyperparameters.
+- Some notebooks found at the extension `ML_Project1/Projet/scripts/.ipynb_checkpoints`, where all implemented methods are put to use, the training of tha data and the producing of the prediction files take place.
+
+### `submission`
+
+`submission` contains a list of `.csv`submission files, consisting of predictions on the given dataset.
---
+++ src/cross_validation.py
@@ -0,0 +1,125 @@
+# -*- coding: utf-8 -*-
+"""cross_validation / choice of hyperparameters"""
+
+
+import numpy as np
+from implementations import *
+from helpers import *
+from helper_implementations import *
+from cleaning_data import *
+import matplotlib.pyplot as plt
+
+#Given from the exercies sessions, give the indices for the k_fold cross validation 
+def build_k_indices(y, k_fold, seed):
+    """Build k indices for k-fold."""
+    
+    num_row = y.shape[0]
+    interval = int(num_row / k_fold)
+    np.random.seed(seed)
+    indices = np.random.permutation(num_row)
+    k_indices = [indices[k * interval: (k + 1) * interval]
+                 for k in range(k_fold)]
+    return np.array(k_indices)
+
+#The below function will plot a boxplot of the accuracy of the least square method with respect to the degree of the polynomial expansion
+def cross_val_LS(y,x,k_fold,max_degree):
+    """Cross validation for the polynomial expansion of the featres for least_squares"""
+   
+    #Initialization
+    seed = 12
+    degrees = np.arange(1,max_degree+1)
+    accuracies= []
+    k_indices = build_k_indices(y,k_fold,seed)
+    
+    #Cross validation
+    for d in degrees:
+        accuracies_tmp= []
+        for i in range(k_fold):
+            
+            #Spliting the data in k-1 train folds and 1 test fold
+            te_indice = k_indices[i]
+            tr_indice = k_indices[~(np.arange(k_indices.shape[0]) == i)]
+            tr_indice = tr_indice.reshape(-1)
+            x_te=x[te_indice]
+            y_te=y[te_indice]
+            x_tr=x[tr_indice]
+            y_tr=y[tr_indice]
+            
+            #Adapting the train data to the problem
+            x_tr0, x_tr1, x_tr23 = adapt_x(x_tr, d)
+            y_tr0, y_tr1, y_tr23 = adapt_y_least_squares(y_tr, x_tr)
+            
+            #Computing the weights for each part of the data
+            w_0, _ = least_squares(y_tr0, x_tr0)
+            w_1, _ = least_squares(y_tr1, x_tr1)
+            w_23, _ = least_squares(y_tr23, x_tr23)
+           
+            #Computing the accuracy for each validation
+            y_pred = label(w_0, w_1, w_23, x_tr0, x_tr1, x_tr23, x_tr, 'least squares')
+            accuracy = compute_accuracy(y_tr, y_pred)
+            accuracies_tmp.append(accuracy)
+            
+        #Saving the accuracy for each degree    
+        accuracies.append(accuracies_tmp)
+    
+    #Printing the result in boxplot form
+    plt.boxplot(accuracies)
+    plt.title('Train error distribution')
+    plt.xlabel('degree')
+    plt.ylabel('accuracy')
+    plt.savefig("cross_validation_LS")
+
+#The below function will return a max_degree*len(gamma)*2 array where the element [d,i,0] is the mean of the accuracy on the k folds for degree d and gamma gamma[i] and [d,i,1] is the std of the accuracy on the k folds for degree d and gamma[i]
+#This hold for the logistic regression method and the degree d is the degree of the polynomial expansion
+
+def cross_val_Log(y,x,k_fold,max_degree,gamma):
+    """Cross validation for the polynomial expansion of the featres for logistic_regression"""
+    
+    #Initialization
+    seed = 1
+    max_iters = 50
+    degrees = np.arange(1,max_degree+1)
+    k_indices = build_k_indices(y,k_fold,seed)
+    mean_std=np.zeros((max_degree,len(gamma),2))
+    
+    #Cross validation
+    for d in degrees:
+        for index,j in enumerate(gamma):
+            accuracies = []
+            for i in range(k_fold):
+                
+                #Spliting the data in k-1 train folds and 1 test fold
+                te_indice = k_indices[i]
+                tr_indice = k_indices[~(np.arange(k_indices.shape[0]) == i)]
+                tr_indice = tr_indice.reshape(-1)
+                x_te=x[te_indice]
+                y_te=y[te_indice]
+                x_tr=x[tr_indice]
+                y_tr=y[tr_indice]
+                   
+                #Adapting the train data to the problem   
+                x_tr0, x_tr1, x_tr23 = adapt_x(x_tr, d)
+                y_tr0, y_tr1, y_tr23 = adapt_y_least_squares(y_tr, x_tr)
+                
+                #Computing the weights for each part of the data
+                w_0, loss_tr0= logistic_regression(y_tr0, x_tr0, np.zeros(x_tr0.shape[1]), max_iters, j)
+                w_1, loss_tr1= logistic_regression(y_tr1, x_tr1, np.zeros(x_tr1.shape[1]), max_iters, j)
+                w_23, loss_tr23 = logistic_regression(y_tr23, x_tr23, np.zeros(x_tr23.shape[1]), max_iters, j)
+           
+                #Computing the accuracy for each validation
+                y_pred = label(w_0, w_1, w_23, x_tr0, x_tr1, x_tr23, x_tr, 'logistic regression')
+                accuracy = compute_accuracy(y_tr, y_pred)
+                accuracies.append(accuracy)
+                
+            #Saving the accuracy for each degree
+            mean_std[d-1,index,0]=np.mean(accuracies)
+            mean_std[d-1,index,1]=np.std(accuracies)
+
+    return mean_std
+    
+
+    
+    
+
+
+
